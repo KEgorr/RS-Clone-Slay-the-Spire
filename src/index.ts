aaf232239707ba37e54edd9eb683d4f4c7a46dc6
Andrei Rassokhin <rasosha@gmail.com>
2023-02-09 00:30:01 +0800

    add: draggable

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,77 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import * as PIXI from 'pixi.js';
 import './assets/css/style.css';
 import './assets/css/reset.css';
 import { Card } from './modules/card-generator';
-import { ironcladCards } from './modules/cards-ironclad';
+// import { ironcladCards } from './modules/cards-ironclad';
+import { colorlessCards } from './modules/cards-colorless';
+
+import { Point } from 'pixi.js';
 
 const app = new PIXI.Application({
-  width: 700,
-  height: 700,
+  width: window.innerWidth - 10,
+  height: window.innerHeight - 10,
   antialias: true,
 });
 
 document.body.appendChild(app.view as HTMLCanvasElement);
-const button = document.createElement('button');
-button.innerHTML = 'add card';
-button.style.width = '200px';
-button.style.height = '200px';
-button.style.transform = 'translateX(-100px)';
-button.style.fontSize = '50px';
 
-button.addEventListener('click', nextCard);
-document.body.appendChild(button);
-
-function nextCard() {
-  // очистить сцену перед добавлением новой карты
-  // while (app.stage.children[0]) {
-  //   app.stage.removeChild(app.stage.children[0]);
-  // }
-  app.stage
-    .addChild(new Card(ironcladCards[Math.floor(Math.random() * 74)]).render().on('pointerdown', onDragStart))
-    .position.set(Math.floor(Math.random() * 350), Math.floor(Math.random() * 250));
-}
-
-let dragTarget: PIXI.DisplayObject;
-
-app.stage.interactive = true;
-app.stage.hitArea = app.screen;
-app.stage.on('pointerup', onDragEnd);
-app.stage.on('pointerupoutside', onDragEnd);
+let dragTarget: PIXI.Container | null;
+let pointerInCard: PIXI.Point;
 
 function onDragMove(event: PIXI.FederatedPointerEvent) {
   if (dragTarget) {
-    // dragTarget.parent.toLocal(event.global, null, dragTarget.position);
-    dragTarget.x = event.client.x - 180;
-    dragTarget.y = event.client.y - 250;
+    const pointerPosition = new Point(event.clientX, event.clientY);
+    dragTarget.x = pointerPosition.x - pointerInCard.x;
+    dragTarget.y = pointerPosition.y - pointerInCard.y;
   }
 }
 
 function onDragStart(event: PointerEvent) {
   dragTarget = event.target as PIXI.Container;
-  app.stage.on('pointermove', onDragMove);
+  dragTarget.zIndex = 1;
+  app.stage.sortChildren();
+  const targetPosition = (event.target as PIXI.Container).position;
+  const pointerPosition = new Point(event.clientX, event.clientY);
+  pointerInCard = new Point(pointerPosition.x - targetPosition.x, pointerPosition.y - targetPosition.y);
+  console.log('pointerInCard', pointerInCard);
 }
 
 function onDragEnd() {
   if (dragTarget) {
-    app.stage.off('pointermove', onDragMove);
+    dragTarget.zIndex = 0;
+    app.stage.sortChildren();
+    dragTarget = null;
   }
 }
+
+function onPointerOver(event: PointerEvent) {
+  (event.target as PIXI.Container).zIndex = 1;
+  app.stage.sortChildren();
+}
+function onPointerOut(event: PointerEvent) {
+  (event.target as PIXI.Container).zIndex = 0;
+  app.stage.sortChildren();
+}
+
+function addCard(n: number) {
+  const arr = new Array(n)
+    .fill(0)
+    .map((item, index) => 0 + index)
+    .reverse();
+  arr.forEach((item) => {
+    const card: PIXI.Container = new Card(colorlessCards[item]).render();
+    card.position.set(200 * item, 500);
+    card.on('pointerdown', onDragStart);
+    card.on('pointerup', onDragEnd);
+    card.on('pointerupoutside', onDragEnd);
+    card.on('pointermove', onDragMove);
+    card.on('pointerover', onPointerOver);
+    card.on('pointerout', onPointerOut);
+    app.stage.addChild(card);
+  });
+}
+
+app.stage.interactive = true;
+addCard(7);
--- src/modules/card-generator.ts
+++ src/modules/card-generator.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable prettier/prettier */
 import * as PIXI from 'pixi.js';
 import { ICard } from '../interfaces';
